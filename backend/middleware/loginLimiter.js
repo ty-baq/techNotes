c56c4cb877ff749779496ffbe2218c5edb28f673
Tayo Bakare <orbakare@outlook.com>
2023-09-07 21:27:12 -0700

    loginLimiter file added

---
+++ backend/middleware/loginLimiter.js
@@ -0,0 +1,19 @@
+const ratelimit = require('express-rate-limit')
+const { logEvents } = require('./logger')
+
+const loginLimiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 5, //limit each IP to 5 login requests per `window` per minute
+    message:
+    {
+        message: 'Too many login attempts from the IP, please try again after a 60 second pause'
+    },
+    handler: (req, res, next, options) => {
+        logEvents(`Too Many Requests: ${optionss.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
+        res.status(options.statusCode).send(options.message)
+    },
+    standardHeaders: true, //Return rate limit info in the `RateLimit-*` 
+    legacyHeaders: false, //Disable the `X-RateLimit-*` headers
+})
+
+module.exports = loginLimiter
